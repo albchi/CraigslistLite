e2cfbf330899bf4802a7250170e75ce5b858d55c
Air Al <albchi@gmail.com>
2016-12-03 12:23:04 -0800

    grammer

--- sweden.php
+++ sweden.php
@@ -7,7 +7,7 @@
    if ($conn->connect_error) {
       die("Failed to connect to MySQL Server " . $conn->connecterror);
    }
-   // print "Success to connect to MySQL Server";
+   // print "Success connecting to MySQL Server";
 
    // echo "<br>";
    // $new_un = $_POST['username']; 
